ce2baaf2006ee9ae9a23d5ad9f91936393a367f8
Clayton <clayton.fortini@gmail.com>
2019-11-18 12:18:57 -0500

    ATM Functionality done

--- src/app/atm/atm.component.ts
+++ src/app/atm/atm.component.ts
@@ -10,6 +10,7 @@ import { BankService } from '../bank.service';
 export class AtmComponent {
   value = 0;
   transDetails = '';
+  trans = this.bankService.account.transactions;
   constructor(public bankService: BankService) { }
 
   withdraw() {
@@ -25,9 +26,24 @@ export class AtmComponent {
     // Display Transaction Details
     this.transDetails =
     `Transaction Successful!
-    Amount ${transactionType}: ${this.value},
-    Account Balance:  ${this.bankService.account.balance}`;
+    Amount ${transactionType}: $${this.value}
+    Account Balance:  $${this.bankService.account.balance}`;
 
     // Add Object to Transaction History
+    this.bankService.account.transactions.push({date: '11-18-2019',
+    type: `${transactionType}`,
+    amount: `${this.value}`,
+    currency: 'usd'});
+  }
+  transactionHistory() {
+    this.transDetails = `Recent Transactions: `;
+    for (let i = this.trans.length - 1; i >= 0; i--) {
+      this.transDetails +=
+      `
+      ${this.bankService.account.transactions[i].date}
+       ${this.trans[i].type}
+      $${this.trans[i].amount}
+       ${this.trans[i].currency}`;
+    }
   }
 }
